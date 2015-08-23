13191781c2bd42d3f2b78fc4bfeb57382fa4bcc7
John Kavanagh <john@kavanista.com>
2015-08-23 17:43:14 +0100

    added mysql config struct

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
+
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'jay'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
